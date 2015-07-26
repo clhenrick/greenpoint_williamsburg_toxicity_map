b7a9d7b790688f6d56969e13654212df1bdac3dd
bowonc <bowon78@hotmail.com>
2015-07-26 12:39:07 -0400

    Mapbox legend removed. Asthma layer CartoCSS changed at polygon-opacity  1->.5 / lines in About tab removed as requested. The subtitles of layers merged :the sites + environmental risks.
    extra findings : zipcode lines should be distinguishable from polygon lines. dotted line recommended.

--- js/interaction.js
+++ js/interaction.js
@@ -1,61 +1,10 @@
 var app = app || {};
 
 app.interaction = (function($, _) {
-    /*
-$('button.next').click(function() {
-    var $target = $($(this).attr('href')),
-        $other = $target.siblings('.active');
-    
-    if (!$target.hasClass('active')) {
-        $other.each(function(index, self) {
-            var $this = $(this);
-            $this.removeClass('active').animate({
-                left: $this.width()
-            }, 500);
-        });
+/* about */
 
-        $target.addClass('active').show().css({
-            left: -($target.width())
-        }).animate({
-            left: 0
-        }, 500);
-    }
-});
-*/
+//about map layers
+//about us 
+//contactus
 
-
-
-    var mouseX = e.pageX; 
-    var mouseY = e.pageY;
-$("#sublayer1").hover(function() {
-    var ids = $(this).attr('id');
-    var name = '.'+ids+'_info';
-    $(name).css({'top':mouseY,'left':mouseX}).toggle();
-    console.log(name);
-    });
-
-    /*
-    <!--pop dense -->
-    <div class="cartodb-popup v2">
-      <a href="#close" class="cartodb-popup-close-button close">x</a>
-      <div class="cartodb-popup-content-wrapper">
-        <div class="cartodb-popup-content">
-          <h4>roundedcpop</h4>
-          <p>{{roundedcpop}}</p>
-        </div>
-      </div>
-      <div class="cartodb-popup-tip-container"></div>
-    </div>
-    <div class="cartodb-tooltip-content-wrapper">
-      <div class="cartodb-tooltip-content">
-        <h4>drovealone</h4>
-        <p>{{drovealone}}</p>
-      </div>
-    </div>
-    <!--popdense hover-->
-    <div class="btn-group-vertical btn-group-xs zoombtn" role="group" aria-label="...">
-      <button type="button" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></button>
-       <button type="button" class="btn btn-default btn-xs"> <span class="glyphicon glyphicon-minus" aria-hidden="true"></span></button>
-    </div>
-    */
 })(jQuery, _);
