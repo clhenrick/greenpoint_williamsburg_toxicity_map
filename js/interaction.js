5c9aebf19839c1936ed9c495cfe5abf41404063e
bowonc <bowon78@hotmail.com>
2015-07-30 08:36:42 -0400

    logo size change
    Hopefully no more cosmetic change, unless major issue appear on interaction. Maybe button changes to on/off format

--- js/interaction.js
+++ js/interaction.js
@@ -2,6 +2,7 @@ var app = app || {};
 
 app.interaction = (function($, _) {
 
+<<<<<<< HEAD
     var desc = [{
         title: "waste_transfer_stations",
         field: "cotents1"
@@ -44,6 +45,47 @@ app.interaction = (function($, _) {
     		}
     	});
     */
+=======
+	var desc = [
+		{title:"waste_transfer_stations", field:"cotents1"},
+		{title:"polluted_points", field:"cotents2"},
+		{title:"flood_risk", field:"cotents3"},
+		{title:"polluted_polygons", field:"cotents3"},
+		{title:"acs_pop", field:"cotents3"},
+		{title:"acs_income", field:"cotents3"},
+		{title:"asthma", field:"cotents3"}
+	];
+	$('.layers').tooltip({
+		content:function(){
+			return "wts tooltip!";
+			}
+		});
+	$('selected').click(function() {
+		$(this).toggleClass('.pressed');
+	}).hover(function() {
+		$(this).toggleClass('.pressed');
+	}).disableSelection();
+	/*
+	var buttonPressed = false;
+	$('selected').click(function(event) {
+		if(buttonPressed == false){
+			buttonPressed = true;
+			('selected').addClass('.pressed');
+		} 
+        else {
+			buttonPressed = false;
+			('selected').removeClass('.pressed');
+		}
+	});
+*/
+	/*
+	$('#'+desc[1].title).tooltip({
+		content:function(){
+			return desc[1].field;
+			}
+		});
+	*/
+>>>>>>> change zoomin button style
     /*  
     function layer_desc() {
 
