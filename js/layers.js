1b1766e56cf2c703aff5e2cf57cb8ba1ab13f3be
Chris Henrick <chrishenrick@gmail.com>
2015-09-10 10:31:51 -0400

    add cartodb_id to each layer that will have interaction

--- js/layers.js
+++ js/layers.js
@@ -21,22 +21,22 @@ app.layers = (function(){
           name: 'asthma',
           sql: "SELECT * FROM asthma_5yr",
           cartocss: cartocss.asthma,
-          interactivity: "asthptot"
+          interactivity: "cartodb_id, asthptot"
       }, {
           name: 'flood_risk',
           sql: "SELECT * FROM floodrisk", 
           cartocss: cartocss.floodrisk,
-          interactivity: ''
+          interactivity: ""
       }, {
           name: 'polluted_polygons',
           sql: "SELECT * FROM polluted_poly_f",
           cartocss: cartocss["polluted-areas"],
-          interactivity: "site_or_owner_name,address,description,program,link_for_more_information,reference_number"
+          interactivity: "cartodb_id, site_or_owner_name,address,description,program,link_for_more_information,reference_number"
       }, {
           name: 'industrial_history_lines',
           sql: "SELECT * FROM industrialhistory_lines",
           cartocss: cartocss["industrial-history-lines"],
-          interactivity: "cartodb_id"
+          interactivity: ""
       }, {
           name: 'industrial_history_points',
           sql: "SELECT * FROM industrialhistory_points",
@@ -46,12 +46,12 @@ app.layers = (function(){
           name: 'polluted_points',
           sql: "SELECT * FROM polluted_points_f",
           cartocss: cartocss["polluted-points"],
-          interactivity: "facility_or_owner, description,type_of_pollution,address,date,agency_program,link_for_more_information,nag_id,quantity,remediated,responding_agency,source"
+          interactivity: "cartodb_id, facility_or_owner, description,type_of_pollution,address,date,agency_program,link_for_more_information,nag_id,quantity,remediated,responding_agency,source"
       }, {
           name: 'waste_transfer_stations',
           sql: "SELECT * FROM wts_July30", 
           cartocss: cartocss.wts,
-          interactivity: "name, facilitytype, address, source"
+          interactivity: "cartodb_id, name, facilitytype, address, source"
       }]
   }
 })();  
