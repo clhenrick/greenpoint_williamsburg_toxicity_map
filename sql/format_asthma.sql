effa5449f2cd9863bb1eb8e92b7bc9fc52272937
Chris Henrick <chrishenrick@gmail.com>
2015-09-28 11:14:15 -0400

    add sql for industrial hist lines & asthma incidents

--- sql/format_asthma.sql
+++ sql/format_asthma.sql
@@ -1,3 +1,5 @@
 -- rename asthma column
 
+-- Update asthma Set asthma5ypop = ( Asthma5y / totpop ) *1000
+-- Update asthma Set rounyed_asthma5ypop = round(asthma5ypop)
 alter table asthma_5yr rename column asthptot to asthma_rate_per_1000_people;
---
+++ sql/format_industrial_history_lines.sql
@@ -0,0 +1,3 @@
+--- is this necessary?
+--- for making industrial historical tour line
+SELECT ST_MakeLine(the_geom order by id ASC) as the_geom_webmercator  FROM industrialhistory
